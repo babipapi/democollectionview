5840aa3641ca661c0059e35f0c7a0a212b1cd815
Daniel <babipapi@gmail.com>
2019-08-08 23:44:26 +0800

    Initial Commit

---
+++ democollectionview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  democollectionview
+//
+//  Created by Daniel on 2019/8/8.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
