68eb7f7ba02d77e01cded59dc41c6c24badde3f6
Daniel <babipapi@gmail.com>
2019-08-10 21:31:09 +0800

    fix1

---
+++ democollectionview/PrinceCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PrinceCollectionViewCell.swift
+//  democollectionview
+//
+//  Created by Daniel on 2019/8/8.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class PrinceCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
